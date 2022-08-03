462d74ef13e89bef2c1609277dc74ff4badf0cf7
Ryan Faerman <ryan@faerman.net>
2022-08-03 16:51:07 -0400

    renamed

---
+++ gcode/marlin/marlin.go
@@ -0,0 +1,39 @@
+package marlin
+
+import (
+	g "github.com/ryanfaerman/gophercnc/gcode"
+)
+
+var (
+	A = g.A
+	B = g.B
+	C = g.C
+	D = g.D
+	E = g.E
+	F = g.F
+	G = g.G
+	H = g.H
+	I = g.I
+	J = g.J
+	K = g.K
+	L = g.L
+	M = g.M
+	N = g.M
+	O = g.O
+	P = g.P
+	Q = g.Q
+	R = g.R
+	S = g.S
+	T = g.T
+	U = g.U
+	V = g.V
+	W = g.W
+	X = g.X
+	Y = g.Y
+	Z = g.Z
+
+	String  = g.String
+	Comment = g.Comment
+
+	G1 = g.Command(G, 0.0, X, Y, Z)
+)
